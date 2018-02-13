24eaacf0efb8825ade5d81506179edb253d6f554
Arturo Torres Sánchez <r2d2.art2005@gmail.com>
2018-02-12 21:22:32 -0600

    RequireStruct and RequireClass are structs now, so that they can be used by others

--- NullableClass/NullableExtensions.cs
+++ NullableClass/NullableExtensions.cs
@@ -5,37 +5,37 @@ using System.Text;
 namespace NullableClass
 {
 
-	public sealed class RequireStruct<T> where T : struct { private RequireStruct() { } }
-	public sealed class RequireClass<T> where T : class { private RequireClass() { } }
+	public struct RequireStruct<T> where T : struct { }
+	public struct RequireClass<T> where T : class { }
 
 	public static class NullableExtensions
 	{
-		public static NullableOf<TResult> Select<T, TResult>(this NullableOf<T> @this, Func<T, TResult> func, RequireClass<TResult> _ = null)
+		public static NullableOf<TResult> Select<T, TResult>(this NullableOf<T> @this, Func<T, TResult> func, RequireClass<TResult> _ = default(RequireClass<TResult>))
 			where T : class
 			where TResult : class
 		{
 			return @this.HasValue ? func((T)@this) : default(NullableOf<TResult>);
 		}
-		public static Nullable<TResult> Select<T, TResult>(this NullableOf<T> @this, Func<T, TResult> func, RequireStruct<TResult> _ = null)
+		public static Nullable<TResult> Select<T, TResult>(this NullableOf<T> @this, Func<T, TResult> func, RequireStruct<TResult> _ = default(RequireStruct<TResult>))
 			where T : class
 			where TResult : struct
 		{
 			return @this.HasValue ? func((T)@this) : default(Nullable<TResult>);
 		}
-		public static NullableOf<TResult> Select<T, TResult>(this Nullable<T> @this, Func<T, TResult> func, RequireClass<TResult> _ = null)
+		public static NullableOf<TResult> Select<T, TResult>(this Nullable<T> @this, Func<T, TResult> func, RequireClass<TResult> _ = default(RequireClass<TResult>))
 			where T : struct
 			where TResult : class
 		{
 			return @this.HasValue ? func((T)@this) : default(NullableOf<TResult>);
 		}
-		public static Nullable<TResult> Select<T, TResult>(this Nullable<T> @this, Func<T, TResult> func, RequireStruct<TResult> _ = null)
+		public static Nullable<TResult> Select<T, TResult>(this Nullable<T> @this, Func<T, TResult> func, RequireStruct<TResult> _ = default(RequireStruct<TResult>))
 			where T : struct
 			where TResult : struct
 		{
 			return @this.HasValue ? func((T)@this) : default(Nullable<TResult>);
 		}
 
-		public static NullableOf<TResult> SelectMany<T, TMid, TResult>(this NullableOf<T> @this, Func<T, NullableOf<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireClass<TResult> _ = null)
+		public static NullableOf<TResult> SelectMany<T, TMid, TResult>(this NullableOf<T> @this, Func<T, NullableOf<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireClass<TResult> _ = default(RequireClass<TResult>))
 			where T : class
 			where TMid : class
 			where TResult : class
@@ -45,7 +45,7 @@ namespace NullableClass
 			if (!mid.HasValue) return default(NullableOf<TResult>);
 			return resultSelector((T)@this, (TMid)mid);
 		}
-		public static Nullable<TResult> SelectMany<T, TMid, TResult>(this NullableOf<T> @this, Func<T, NullableOf<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireStruct<TResult> _ = null)
+		public static Nullable<TResult> SelectMany<T, TMid, TResult>(this NullableOf<T> @this, Func<T, NullableOf<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireStruct<TResult> _ = default(RequireStruct<TResult>))
 			where T : class
 			where TMid : class
 			where TResult : struct
@@ -55,7 +55,7 @@ namespace NullableClass
 			if (!mid.HasValue) return default(Nullable<TResult>);
 			return resultSelector((T)@this, (TMid)mid);
 		}
-		public static NullableOf<TResult> SelectMany<T, TMid, TResult>(this NullableOf<T> @this, Func<T, Nullable<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireClass<TResult> _ = null)
+		public static NullableOf<TResult> SelectMany<T, TMid, TResult>(this NullableOf<T> @this, Func<T, Nullable<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireClass<TResult> _ = default(RequireClass<TResult>))
 			where T : class
 			where TMid : struct
 			where TResult : class
@@ -65,7 +65,7 @@ namespace NullableClass
 			if (!mid.HasValue) return default(NullableOf<TResult>);
 			return resultSelector((T)@this, (TMid)mid);
 		}
-		public static Nullable<TResult> SelectMany<T, TMid, TResult>(this NullableOf<T> @this, Func<T, Nullable<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireStruct<TResult> _ = null)
+		public static Nullable<TResult> SelectMany<T, TMid, TResult>(this NullableOf<T> @this, Func<T, Nullable<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireStruct<TResult> _ = default(RequireStruct<TResult>))
 			where T : class
 			where TMid : struct
 			where TResult : struct
@@ -75,7 +75,7 @@ namespace NullableClass
 			if (!mid.HasValue) return default(Nullable<TResult>);
 			return resultSelector((T)@this, (TMid)mid);
 		}
-		public static NullableOf<TResult> SelectMany<T, TMid, TResult>(this Nullable<T> @this, Func<T, NullableOf<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireClass<TResult> _ = null)
+		public static NullableOf<TResult> SelectMany<T, TMid, TResult>(this Nullable<T> @this, Func<T, NullableOf<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireClass<TResult> _ = default(RequireClass<TResult>))
 			where T : struct
 			where TMid : class
 			where TResult : class
@@ -85,7 +85,7 @@ namespace NullableClass
 			if (!mid.HasValue) return default(NullableOf<TResult>);
 			return resultSelector((T)@this, (TMid)mid);
 		}
-		public static Nullable<TResult> SelectMany<T, TMid, TResult>(this Nullable<T> @this, Func<T, NullableOf<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireStruct<TResult> _ = null)
+		public static Nullable<TResult> SelectMany<T, TMid, TResult>(this Nullable<T> @this, Func<T, NullableOf<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireStruct<TResult> _ = default(RequireStruct<TResult>))
 			where T : struct
 			where TMid : class
 			where TResult : struct
@@ -95,7 +95,7 @@ namespace NullableClass
 			if (!mid.HasValue) return default(Nullable<TResult>);
 			return resultSelector((T)@this, (TMid)mid);
 		}
-		public static NullableOf<TResult> SelectMany<T, TMid, TResult>(this Nullable<T> @this, Func<T, Nullable<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireClass<TResult> _ = null)
+		public static NullableOf<TResult> SelectMany<T, TMid, TResult>(this Nullable<T> @this, Func<T, Nullable<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireClass<TResult> _ = default(RequireClass<TResult>))
 			where T : struct
 			where TMid : struct
 			where TResult : class
@@ -105,7 +105,7 @@ namespace NullableClass
 			if (!mid.HasValue) return default(NullableOf<TResult>);
 			return resultSelector((T)@this, (TMid)mid);
 		}
-		public static Nullable<TResult> SelectMany<T, TMid, TResult>(this Nullable<T> @this, Func<T, Nullable<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireStruct<TResult> _ = null)
+		public static Nullable<TResult> SelectMany<T, TMid, TResult>(this Nullable<T> @this, Func<T, Nullable<TMid>> midSelector, Func<T, TMid, TResult> resultSelector, RequireStruct<TResult> _ = default(RequireStruct<TResult>))
 			where T : struct
 			where TMid : struct
 			where TResult : struct
