822a5efb822a30d9bd1aee2301b5ec805880f8fd
Arturo Torres Sánchez <r2d2.art2005@gmail.com>
2018-03-10 00:11:44 -0600

    Enumerable extensions (anything with Default)

--- NullableClass.Test/NullableClassTests.cs
+++ NullableClass.Test/NullableClassTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MSTestExtensions;
 using NullableClass;
@@ -97,6 +99,230 @@ namespace NullableClass.Test
 
 			// This is false because the declared types are incompatible
 			Assert.IsFalse(nullableString.Equals(nullableObject));
+		}
+
+		[TestMethod]
+		public void DefaultIfEmpty_WithEmptyLists()
+		{
+			var list1 = new List<string	>();
+			var list2 = new List<int	>();
+			var list3 = new List<NullableOf<string>	>();
+			var list4 = new List<Nullable<int>	>();
+			
+			Assert.AreEqual(""	, list1.DefaultIfEmpty_().First());
+			Assert.AreEqual(0	, list2.DefaultIfEmpty_().First());
+			Assert.AreEqual(null	, list3.DefaultIfEmpty_().First());
+			Assert.AreEqual(null	, list4.DefaultIfEmpty_().First());
+		}
+
+		[TestMethod]
+		public void DefaultIfEmpty_WithFilledLists()
+		{
+			var list1 = new List<string	> { "a"	};
+			var list2 = new List<int	> { 5	};
+			var list3 = new List<NullableOf<string>	> { "a"	};
+			var list4 = new List<Nullable<int>	> { 5	};
+
+			Assert.AreEqual("a"	, list1.DefaultIfEmpty_().First());
+			Assert.AreEqual(5	, list2.DefaultIfEmpty_().First());
+			Assert.AreEqual("a"	, list3.DefaultIfEmpty_().First());
+			Assert.AreEqual(5	, list4.DefaultIfEmpty_().First());
+		}
+
+		[TestMethod]
+		public void ElementAtOrDefault_WithEmptyLists()
+		{
+			var list1 = new List<string	>();
+			var list2 = new List<int	>();
+			var list3 = new List<NullableOf<string>	>();
+			var list4 = new List<Nullable<int>	>();
+			
+			Assert.AreEqual(""	, list1.ElementAtOrDefault_(10));
+			Assert.AreEqual(0	, list2.ElementAtOrDefault_(10));
+			Assert.AreEqual(null	, list3.ElementAtOrDefault_(10));
+			Assert.AreEqual(null	, list4.ElementAtOrDefault_(10));
+		}
+
+		[TestMethod]
+		public void ElementAtOrDefault_WithFilledLists()
+		{
+			var list1 = new List<string	> { "a"	, "b"	};
+			var list2 = new List<int	> { 5	, 10	};
+			var list3 = new List<NullableOf<string>	> { "a"	, "b"	};
+			var list4 = new List<Nullable<int>	> { 5	, 10	};
+
+			Assert.AreEqual("b"	, list1.ElementAtOrDefault_(1));
+			Assert.AreEqual(10	, list2.ElementAtOrDefault_(1));
+			Assert.AreEqual("b"	, list3.ElementAtOrDefault_(1));
+			Assert.AreEqual(10	, list4.ElementAtOrDefault_(1));
+		}
+
+		[TestMethod]
+		public void FirstOrDefault_WithEmptyLists()
+		{
+			var list1 = new List<string	>();
+			var list2 = new List<int	>();
+			var list3 = new List<NullableOf<string>	>();
+			var list4 = new List<Nullable<int>	>();
+			
+			Assert.AreEqual(""	, list1.FirstOrDefault_());
+			Assert.AreEqual(0	, list2.FirstOrDefault_());
+			Assert.AreEqual(null	, list3.FirstOrDefault_());
+			Assert.AreEqual(null	, list4.FirstOrDefault_());
+		}
+
+		[TestMethod]
+		public void FirstOrDefault_WithFilledLists()
+		{
+			var list1 = new List<string	> { "a"	, "b"	};
+			var list2 = new List<int	> { 5	, 10	};
+			var list3 = new List<NullableOf<string>	> { "a"	, "b"	};
+			var list4 = new List<Nullable<int>	> { 5	, 10	};
+
+			Assert.AreEqual("a"	, list1.FirstOrDefault_());
+			Assert.AreEqual(5	, list2.FirstOrDefault_());
+			Assert.AreEqual("a"	, list3.FirstOrDefault_());
+			Assert.AreEqual(5	, list4.FirstOrDefault_());
+		}
+
+		[TestMethod]
+		public void FirstOrDefault_WithFalsePredicate()
+		{
+			var list1 = new List<string	> { "a"	, "b"	};
+			var list2 = new List<int	> { 5	, 10	};
+			var list3 = new List<NullableOf<string>	> { "a"	, "b"	};
+			var list4 = new List<Nullable<int>	> { 5	, 10	};
+			
+			Assert.AreEqual(""	, list1.FirstOrDefault_(x => x == "c"	));
+			Assert.AreEqual(0	, list2.FirstOrDefault_(x => x == 9	));
+			Assert.AreEqual(null	, list3.FirstOrDefault_(x => x == "c"	));
+			Assert.AreEqual(null	, list4.FirstOrDefault_(x => x == 9	));
+		}
+
+		[TestMethod]
+		public void FirstOrDefault_WithTruePredicate()
+		{
+			var list1 = new List<string	> { "a"	, "b"	};
+			var list2 = new List<int	> { 5	, 10	};
+			var list3 = new List<NullableOf<string>	> { "a"	, "b"	};
+			var list4 = new List<Nullable<int>	> { 5	, 10	};
+
+			Assert.AreEqual("a"	, list1.FirstOrDefault_(x => x == "a"	));
+			Assert.AreEqual(5	, list2.FirstOrDefault_(x => x == 5	));
+			Assert.AreEqual("a"	, list3.FirstOrDefault_(x => x == "a"	));
+			Assert.AreEqual(5	, list4.FirstOrDefault_(x => x == 5	));
+		}
+
+		[TestMethod]
+		public void SingleOrDefault_WithEmptyLists()
+		{
+			var list1 = new List<string	>();
+			var list2 = new List<int	>();
+			var list3 = new List<NullableOf<string>	>();
+			var list4 = new List<Nullable<int>	>();
+			
+			Assert.AreEqual(""	, list1.SingleOrDefault_());
+			Assert.AreEqual(0	, list2.SingleOrDefault_());
+			Assert.AreEqual(null	, list3.SingleOrDefault_());
+			Assert.AreEqual(null	, list4.SingleOrDefault_());
+		}
+
+		[TestMethod]
+		public void SingleOrDefault_WithFilledLists()
+		{
+			var list1 = new List<string	> { "a"	};
+			var list2 = new List<int	> { 5	};
+			var list3 = new List<NullableOf<string>	> { "a"	};
+			var list4 = new List<Nullable<int>	> { 5	};
+
+			Assert.AreEqual("a"	, list1.SingleOrDefault_());
+			Assert.AreEqual(5	, list2.SingleOrDefault_());
+			Assert.AreEqual("a"	, list3.SingleOrDefault_());
+			Assert.AreEqual(5	, list4.SingleOrDefault_());
+		}
+
+		[TestMethod]
+		public void SingleOrDefault_WithFalsePredicate()
+		{
+			var list1 = new List<string	> { "a"	, "b"	};
+			var list2 = new List<int	> { 5	, 10	};
+			var list3 = new List<NullableOf<string>	> { "a"	, "b"	};
+			var list4 = new List<Nullable<int>	> { 5	, 10	};
+			
+			Assert.AreEqual(""	, list1.SingleOrDefault_(x => x == "c"	));
+			Assert.AreEqual(0	, list2.SingleOrDefault_(x => x == 9	));
+			Assert.AreEqual(null	, list3.SingleOrDefault_(x => x == "c"	));
+			Assert.AreEqual(null	, list4.SingleOrDefault_(x => x == 9	));
+		}
+
+		[TestMethod]
+		public void SingleOrDefault_WithTruePredicate()
+		{
+			var list1 = new List<string	> { "a"	, "b"	};
+			var list2 = new List<int	> { 5	, 10	};
+			var list3 = new List<NullableOf<string>	> { "a"	, "b"	};
+			var list4 = new List<Nullable<int>	> { 5	, 10	};
+
+			Assert.AreEqual("a"	, list1.SingleOrDefault_(x => x == "a"	));
+			Assert.AreEqual(5	, list2.SingleOrDefault_(x => x == 5	));
+			Assert.AreEqual("a"	, list3.SingleOrDefault_(x => x == "a"	));
+			Assert.AreEqual(5	, list4.SingleOrDefault_(x => x == 5	));
+		}
+
+		[TestMethod]
+		public void LastOrDefault_WithEmptyLists()
+		{
+			var list1 = new List<string	>();
+			var list2 = new List<int	>();
+			var list3 = new List<NullableOf<string>	>();
+			var list4 = new List<Nullable<int>	>();
+			
+			Assert.AreEqual(""	, list1.LastOrDefault_());
+			Assert.AreEqual(0	, list2.LastOrDefault_());
+			Assert.AreEqual(null	, list3.LastOrDefault_());
+			Assert.AreEqual(null	, list4.LastOrDefault_());
+		}
+
+		[TestMethod]
+		public void LastOrDefault_WithFilledLists()
+		{
+			var list1 = new List<string	> { "a"	, "b"	};
+			var list2 = new List<int	> { 5	, 10	};
+			var list3 = new List<NullableOf<string>	> { "a"	, "b"	};
+			var list4 = new List<Nullable<int>	> { 5	, 10	};
+
+			Assert.AreEqual("b"	, list1.LastOrDefault_());
+			Assert.AreEqual(10	, list2.LastOrDefault_());
+			Assert.AreEqual("b"	, list3.LastOrDefault_());
+			Assert.AreEqual(10	, list4.LastOrDefault_());
+		}
+
+		[TestMethod]
+		public void LastOrDefault_WithFalsePredicate()
+		{
+			var list1 = new List<string	> { "a"	, "b"	};
+			var list2 = new List<int	> { 5	, 10	};
+			var list3 = new List<NullableOf<string>	> { "a"	, "b"	};
+			var list4 = new List<Nullable<int>	> { 5	, 10	};
+			
+			Assert.AreEqual(""	, list1.LastOrDefault_(x => x == "c"	));
+			Assert.AreEqual(0	, list2.LastOrDefault_(x => x == 9	));
+			Assert.AreEqual(null	, list3.LastOrDefault_(x => x == "c"	));
+			Assert.AreEqual(null	, list4.LastOrDefault_(x => x == 9	));
+		}
+
+		[TestMethod]
+		public void LastOrDefault_WithTruePredicate()
+		{
+			var list1 = new List<string	> { "a"	, "b"	};
+			var list2 = new List<int	> { 5	, 10	};
+			var list3 = new List<NullableOf<string>	> { "a"	, "b"	};
+			var list4 = new List<Nullable<int>	> { 5	, 10	};
+
+			Assert.AreEqual("a"	, list1.LastOrDefault_(x => x == "a"	));
+			Assert.AreEqual(5	, list2.LastOrDefault_(x => x == 5	));
+			Assert.AreEqual("a"	, list3.LastOrDefault_(x => x == "a"	));
+			Assert.AreEqual(5	, list4.LastOrDefault_(x => x == 5	));
 		}
 	}
 }
---
+++ NullableClass/EnumerableExtensions.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NullableClass;
+
+namespace NullableClass
+{
+	public static class EnumerableExtensions
+	{
+		public static IEnumerable<TSource> DefaultIfEmpty<TSource>(this IEnumerable<TSource> source)
+		{
+			throw new NotImplementedException();
+		}
+		public static IEnumerable<TSource> DefaultIfEmpty_<TSource>(this IEnumerable<TSource> source, RequireClass<TSource> _ = default(RequireClass<TSource>))
+			where TSource : class
+		{
+			return source.DefaultIfEmpty(Default<TSource>.Get());
+		}
+		public static IEnumerable<TSource> DefaultIfEmpty_<TSource>(this IEnumerable<TSource> source, RequireStruct<TSource> _ = default(RequireStruct<TSource>))
+			where TSource : struct
+		{
+			return source.DefaultIfEmpty(default(TSource));
+		}
+		public static IEnumerable<NullableOf<TSource>> DefaultIfEmpty_<TSource>(this IEnumerable<NullableOf<TSource>> source)
+			where TSource : class
+		{
+			return source.DefaultIfEmpty(null);
+		}
+		public static IEnumerable<Nullable<TSource>> DefaultIfEmpty_<TSource>(this IEnumerable<Nullable<TSource>> source)
+			where TSource : struct
+		{
+			return source.DefaultIfEmpty(null);
+		}
+		
+		public static TSource ElementAtOrDefault<TSource>(this IEnumerable<TSource> source, int index)
+		{
+			throw new NotImplementedException();
+		}
+		public static TSource ElementAtOrDefault_<TSource>(this IEnumerable<TSource> source, int index, RequireClass<TSource> _ = default(RequireClass<TSource>))
+			where TSource : class
+		{
+			return Enumerable.ElementAtOrDefault(source, index) ?? Default<TSource>.Get();
+		}
+		public static TSource ElementAtOrDefault_<TSource>(this IEnumerable<TSource> source, int index, RequireStruct<TSource> _ = default(RequireStruct<TSource>))
+			where TSource : struct
+		{
+			return Enumerable.ElementAtOrDefault(source, index);
+		}
+		public static NullableOf<TSource> ElementAtOrDefault_<TSource>(this IEnumerable<NullableOf<TSource>> source, int index)
+			where TSource : class
+		{
+			return Enumerable.ElementAtOrDefault(source, index);
+		}
+		public static Nullable<TSource> ElementAtOrDefault_<TSource>(this IEnumerable<Nullable<TSource>> source, int index)
+			where TSource : struct
+		{
+			return Enumerable.ElementAtOrDefault(source, index);
+		}
+
+		public static TSource FirstOrDefault<TSource>(this IEnumerable<TSource> source)
+		{
+			throw new NotImplementedException();
+		}
+		public static TSource FirstOrDefault_<TSource>(this IEnumerable<TSource> source, RequireClass<TSource> _ = default(RequireClass<TSource>))
+			where TSource : class
+		{
+			return Enumerable.FirstOrDefault(source) ?? Default<TSource>.Get();
+		}
+		public static TSource FirstOrDefault_<TSource>(this IEnumerable<TSource> source, RequireStruct<TSource> _ = default(RequireStruct<TSource>))
+			where TSource : struct
+		{
+			return Enumerable.FirstOrDefault(source);
+		}
+		public static NullableOf<TSource> FirstOrDefault_<TSource>(this IEnumerable<NullableOf<TSource>> source)
+			where TSource : class
+		{
+			return Enumerable.FirstOrDefault(source);
+		}
+		public static Nullable<TSource> FirstOrDefault_<TSource>(this IEnumerable<Nullable<TSource>> source)
+			where TSource : struct
+		{
+			return Enumerable.FirstOrDefault(source);
+		}
+		
+		public static TSource LastOrDefault<TSource>(this IEnumerable<TSource> source)
+		{
+			throw new NotImplementedException();
+		}
+		public static TSource LastOrDefault_<TSource>(this IEnumerable<TSource> source, RequireClass<TSource> _ = default(RequireClass<TSource>))
+			where TSource : class
+		{
+			return Enumerable.LastOrDefault(source) ?? Default<TSource>.Get();
+		}
+		public static TSource LastOrDefault_<TSource>(this IEnumerable<TSource> source, RequireStruct<TSource> _ = default(RequireStruct<TSource>))
+			where TSource : struct
+		{
+			return Enumerable.LastOrDefault(source);
+		}
+		public static NullableOf<TSource> LastOrDefault_<TSource>(this IEnumerable<NullableOf<TSource>> source)
+			where TSource : class
+		{
+			return Enumerable.LastOrDefault(source);
+		}
+		public static Nullable<TSource> LastOrDefault_<TSource>(this IEnumerable<Nullable<TSource>> source)
+			where TSource : struct
+		{
+			return Enumerable.LastOrDefault(source);
+		}
+		
+		public static TSource SingleOrDefault<TSource>(this IEnumerable<TSource> source)
+		{
+			throw new NotImplementedException();
+		}
+		public static TSource SingleOrDefault_<TSource>(this IEnumerable<TSource> source, RequireClass<TSource> _ = default(RequireClass<TSource>))
+			where TSource : class
+		{
+			return Enumerable.SingleOrDefault(source) ?? Default<TSource>.Get();
+		}
+		public static TSource SingleOrDefault_<TSource>(this IEnumerable<TSource> source, RequireStruct<TSource> _ = default(RequireStruct<TSource>))
+			where TSource : struct
+		{
+			return Enumerable.SingleOrDefault(source);
+		}
+		public static NullableOf<TSource> SingleOrDefault_<TSource>(this IEnumerable<NullableOf<TSource>> source)
+			where TSource : class
+		{
+			return Enumerable.SingleOrDefault(source);
+		}
+		public static Nullable<TSource> SingleOrDefault_<TSource>(this IEnumerable<Nullable<TSource>> source)
+			where TSource : struct
+		{
+			return Enumerable.SingleOrDefault(source);
+		}
+
+		public static TSource FirstOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+		{
+			throw new NotImplementedException();
+		}
+		public static TSource FirstOrDefault_<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, RequireClass<TSource> _ = default(RequireClass<TSource>))
+			where TSource : class
+		{
+			return Enumerable.FirstOrDefault(source, predicate) ?? Default<TSource>.Get();
+		}
+		public static TSource FirstOrDefault_<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, RequireStruct<TSource> _ = default(RequireStruct<TSource>))
+			where TSource : struct
+		{
+			return Enumerable.FirstOrDefault(source, predicate);
+		}
+		public static NullableOf<TSource> FirstOrDefault_<TSource>(this IEnumerable<NullableOf<TSource>> source, Func<NullableOf<TSource>, bool> predicate)
+			where TSource : class
+		{
+			return Enumerable.FirstOrDefault(source, predicate);
+		}
+		public static Nullable<TSource> FirstOrDefault_<TSource>(this IEnumerable<Nullable<TSource>> source, Func<Nullable<TSource>, bool> predicate)
+			where TSource : struct
+		{
+			return Enumerable.FirstOrDefault(source, predicate);
+		}
+
+		public static TSource LastOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+		{
+			throw new NotImplementedException();
+		}
+		public static TSource LastOrDefault_<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, RequireClass<TSource> _ = default(RequireClass<TSource>))
+			where TSource : class
+		{
+			return Enumerable.LastOrDefault(source, predicate) ?? Default<TSource>.Get();
+		}
+		public static TSource LastOrDefault_<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, RequireStruct<TSource> _ = default(RequireStruct<TSource>))
+			where TSource : struct
+		{
+			return Enumerable.LastOrDefault(source, predicate);
+		}
+		public static NullableOf<TSource> LastOrDefault_<TSource>(this IEnumerable<NullableOf<TSource>> source, Func<NullableOf<TSource>, bool> predicate)
+			where TSource : class
+		{
+			return Enumerable.LastOrDefault(source, predicate);
+		}
+		public static Nullable<TSource> LastOrDefault_<TSource>(this IEnumerable<Nullable<TSource>> source, Func<Nullable<TSource>, bool> predicate)
+			where TSource : struct
+		{
+			return Enumerable.LastOrDefault(source, predicate);
+		}
+
+		public static TSource SingleOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+		{
+			throw new NotImplementedException();
+		}
+		public static TSource SingleOrDefault_<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, RequireClass<TSource> _ = default(RequireClass<TSource>))
+			where TSource : class
+		{
+			return Enumerable.SingleOrDefault(source, predicate) ?? Default<TSource>.Get();
+		}
+		public static TSource SingleOrDefault_<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, RequireStruct<TSource> _ = default(RequireStruct<TSource>))
+			where TSource : struct
+		{
+			return Enumerable.SingleOrDefault(source, predicate);
+		}
+		public static NullableOf<TSource> SingleOrDefault_<TSource>(this IEnumerable<NullableOf<TSource>> source, Func<NullableOf<TSource>, bool> predicate)
+			where TSource : class
+		{
+			return Enumerable.SingleOrDefault(source, predicate);
+		}
+		public static Nullable<TSource> SingleOrDefault_<TSource>(this IEnumerable<Nullable<TSource>> source, Func<Nullable<TSource>, bool> predicate)
+			where TSource : struct
+		{
+			return Enumerable.SingleOrDefault(source, predicate);
+		}
+	}
+}
